e62d3a8d7d92a4d07da12e89eef90fcf35294b50
Maksym Kutsos <kutsosmaksym@gmail.com>
2023-09-27 21:28:39 +0200

    HW7 Exchange trycatch for division method for double to ifelse block

--- src/main/java/HW7/Calculator.java
+++ src/main/java/HW7/Calculator.java
@@ -1,41 +1,44 @@
 package HW7;
 
 public class Calculator {
-    static int add (int a, int b){
-        return a+b;
-    }
-    static double add (double a, double b){
-        return a+b;
+    static int add(int a, int b) {
+        return a + b;
     }
 
-    static int subtract (int a, int b){
-        return a-b;
-    }
-    static double subtract (double a, double b){
-        return a-b;
+    static double add(double a, double b) {
+        return a + b;
     }
 
-    static int multiply (int a, int b){
-        return a*b;
-    }
-    static double multiply (double a, double b) {
-        return a*b;
+    static int subtract(int a, int b) {
+        return a - b;
     }
 
-    static int divide (int a, int b) {
+    static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    static int divide(int a, int b) {
         try {
-            return a/b;
-        }
-        catch (ArithmeticException e){
+            return a / b;
+        } catch (ArithmeticException e) {
             return 0;
         }
     }
-    static double divide (double a, double b) {
-        try {
-            return a/b;
-        }
-        catch (ArithmeticException e){
+
+    static double divide(double a, double b) {
+        if (b == 0) {
             return 0;
         }
+        return a / b;
     }
+
+
 }
