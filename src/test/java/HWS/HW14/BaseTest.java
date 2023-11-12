56505ffacc1d9d0a07eea002a285c0355757f2c2
Maksym Kutsos <kutsosmaksym@gmail.com>
2023-11-12 12:29:23 +0100

    HW14

---
+++ src/test/java/HWS/HW14/BaseTest.java
@@ -0,0 +1,40 @@
+package HWS.HW14;
+
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void beforeClass(ITestContext context) {
+        String className = this.getClass().getSimpleName();
+        System.out.println(className + " tests are started");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        String className = this.getClass().getSimpleName();
+        System.out.println(className + " tests are finished");
+    }
+
+    @BeforeMethod
+    public void beforeTestMethod(ITestResult result) {
+        String testName = result.getMethod().getMethodName();
+        System.out.println("Before method for test: " + testName);
+    }
+
+    @AfterMethod
+    public void afterTestMethod(ITestResult result) {
+        String testName = result.getMethod().getMethodName();
+        System.out.println("After method for test: " + testName);
+        if (result.getStatus() == ITestResult.FAILURE) {
+            System.out.println("Test failed!");
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            System.out.println("Test passed!");
+        }
+    }
+}
---
+++ src/test/java/HWS/HW14/PhoneValidatorTest.java
@@ -0,0 +1,55 @@
+package HWS.HW14;
+
+import HWS.HW9.PhoneValidator;
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+public class PhoneValidatorTest extends BaseTest {
+
+    @Test(dataProvider = "validPhones", groups = {"PhoneValidator", "positive"})
+    void checkValidPhones(String validPhone) {
+        assertTrue(PhoneValidator.isPhoneValid(validPhone));
+    }
+
+    @DataProvider
+    public Object[] validPhones() {
+        return new Object[]{
+                "+38(097)9388711",
+                "+38(000)0000000",
+                "+38(099)9999999",
+        };
+    }
+
+    @Test(dataProvider = "invalidPhone", groups = {"PhoneValidator", "negative"})
+    void checkInvalidPhones(String invalidPhone) {
+        assertFalse(PhoneValidator.isPhoneValid(invalidPhone));
+    }
+
+    @DataProvider
+    public Object[] invalidPhone() {
+        return new Object[]{
+                "+380979388711",
+                "38(000)0000000",
+                "+38(09)99999999",
+                "+380(97)9388711",
+                "+38(097)938871",
+                "+38(097)93887112",
+                "+3(099)89999999",
+                "+3(8099)9999999"
+        };
+    }
+
+    @Test(dataProvider = "emptyPhones", groups = {"PhoneValidator", "negative", "empty", "null"})
+    void checkEmptyPhones(String emptyPhones) {
+        assertFalse(PhoneValidator.isPhoneValid(emptyPhones));
+    }
+
+    @DataProvider
+    public Object[] emptyPhones() {
+        return new Object[]{
+                "",
+                null
+        };
+    }
+}
