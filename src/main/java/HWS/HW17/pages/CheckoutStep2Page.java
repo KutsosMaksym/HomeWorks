a348c2f1ca9193e95098ecada635fe10464035bd
KutsosMaksym <32779668+KutsosMaksym@users.noreply.github.com>
2023-11-26 19:46:42 +0100

    HW18 (#13)
    * HW10
    
    * HW10
    
    * HW11
    
    * HW11
    
    * HW11
    
    * HW11
    
    * HW11
    
    * HW11
    
    * HW12
    
    * HW12
    
    * HW12
    
    * HW12
    
    * HW14 Befor-After Classes Methods fix
    
    * HW15
    
    * HW15
    
    * HW15 Method for product search added. Changes in the expected results.
    
    * HW16
    
    * HW18_Refactoring 17 + new test
    
    * HW18_Refactoring 17 + new test
    - Removed hardCode data.
    - Renamed Pages.
    - Added checks by Product Title

---
+++ src/main/java/HWS/HW17/pages/CheckoutStep2Page.java
@@ -0,0 +1,21 @@
+package HWS.HW17.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutStep2Page extends BasePage {
+    public CheckoutStep2Page(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getFirstItemTitle (){
+        return driver.findElement(By.cssSelector(".inventory_item_name")).getText();
+    }
+    public String getTotalPrice() {
+        return driver.findElement(By.xpath("//div[@class='summary_subtotal_label']")).getText();
+    }
+
+    public void finish() {
+        clickButton(driver.findElement(By.cssSelector("#finish")));
+    }
+}
---
+++ src/main/java/HWS/HW17/pages/LoginPage.java
@@ -0,0 +1,33 @@
+package HWS.HW17.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void openLoginPage() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    private void setStandardUserName(String username) {
+        setText(driver.findElement(By.cssSelector("#user-name")), username);
+    }
+
+    public void setCommonPassword(String password) {
+        setText(driver.findElement(By.cssSelector("#password")), password);
+    }
+
+    public void submit() {
+        clickButton(driver.findElement(By.cssSelector("#login-button")));
+    }
+
+    public void login(String username, String password) {
+        setStandardUserName(username);
+        setCommonPassword(password);
+        submit();
+    }
+
+}
